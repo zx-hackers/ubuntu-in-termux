90b85ef40a07446fdfd49c554d5105c0505ffc51
shailesh-zx <138345328+zx-hackers@users.noreply.github.com>
2024-05-04 11:58:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 apt update && apt upgrade -y
-chmod +x root
-mv root /data/data/com.termux/files/usr/bin/
+chmod +x ubuntu
+mv ubuntu /data/data/com.termux/files/usr/bin/
 cd
 mv ubuntu-in-termux ubuntu
 cd ubuntu
